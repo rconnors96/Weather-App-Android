f4c9ca7ed9943856783614c666cd5809cc9b60b7
Ryan <r3connors@student.bridgew.edu>
2020-07-29 11:50:02 +0900

    initial commit

---
+++ app/src/main/java/com/ryanconnors/weatherapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ryanconnors.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //weatherAPI key : fe965d9067db07a703d64d8902e536ad
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
